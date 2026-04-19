e7c2639aca088a8f4a0953eb4e44d27f115b481f
Blaž Perić <blaz.peric4@gmail.com>
2026-04-19 23:35:23 +0200

    .

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -eu
 
 cat <<EOF >/usr/share/nginx/html/env-config.js
 window.__APP_CONFIG__ = {
-  VITE_BACKEND_URL: "${VITE_BACKEND_URL:-http://localhost:3000}"
+  VITE_BACKEND_URL: "${VITE_BACKEND_URL:-}"
 };
 EOF
